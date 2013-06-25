6813e0d5aead5f8b62eab5d1018b7a54683d56e8
ggautier <gaetan.gautier@etu.univ-angers.fr>
2013-06-25 18:57:57 +0200

    Completed architectural model (access to components and interfaces elements)

--- src/fr/univ_nantes/alma/archtool/architectureModel/Component.java
+++ src/fr/univ_nantes/alma/archtool/architectureModel/Component.java
@@ -1,13 +1,106 @@
 package fr.univ_nantes.alma.archtool.architectureModel;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import fr.univ_nantes.alma.archtool.sourceModel.Function;
+import fr.univ_nantes.alma.archtool.sourceModel.Type;
+import fr.univ_nantes.alma.archtool.sourceModel.Variable;
 
 public class Component
 {
-    private Set<Interface> requiredInterfaces;
+    private final Set<Interface> requiredInterfaces = new HashSet<Interface>();
+
+    private final Set<Interface> providedInterfaces = new HashSet<Interface>();
+
+    private final Set<Function> functions = new HashSet<Function>();
+
+    private final Set<Variable> variables = new HashSet<Variable>();
+
+    private final Set<Type> types = new HashSet<Type>();
+
+    public Set<Function> getFunctions()
+    {
+        return new HashSet<Function>(this.functions);
+    }
     
-    private Set<Interface> providedInterfaces;
+    public boolean constainsFunction(Function fct)
+    {
+        return this.functions.contains(fct);
+    }
     
-    // private Set<CodeEntity> codeEntities;
+    public boolean addFunction(final Function fct)
+    {
+        return this.functions.add(fct);
+    }
+
+    public boolean addFunctions(final Set<Function> fcts)
+    {
+        return this.functions.addAll(fcts);
+    }
+    
+    public Set<Variable> getVariables()
+    {
+        return new HashSet<Variable>(this.variables);
+    }
+    
+    public boolean constainsVariable(Function fct)
+    {
+        return this.functions.contains(fct);
+    }
+
+    public boolean addVariable(final Variable var)
+    {
+        return this.variables.add(var);
+    }
+
+    public boolean addVariables(final Set<Variable> vars)
+    {
+        return this.variables.addAll(vars);
+    }
+
+    public Set<Type> getTypes()
+    {
+        return new HashSet<Type>(this.types);
+    }
+
+    public boolean addType(final Type type)
+    {
+        return this.types.add(type);
+    }
+
+    public boolean addTypes(final Set<Type> types)
+    {
+        return this.types.addAll(types);
+    }
+
+    public Set<Interface> getRequiredInterfaces()
+    {
+        return new HashSet<Interface>(this.requiredInterfaces);
+    }
+    
+    public boolean addRequiredInterface(final Interface i)
+    {
+        return this.requiredInterfaces.add(i);
+    }
+
+    public boolean removeRequiredInterface(final Interface i)
+    {
+        return this.requiredInterfaces.remove(i);
+    }
+
+    public Set<Interface> getProvidedInterfaces()
+    {
+        return new HashSet<Interface>(this.providedInterfaces);
+    }
+
+    public boolean addProvidedInterface(final Interface i)
+    {
+        return this.providedInterfaces.add(i);
+    }
+
+    public boolean removeProvidedInterface(final Interface i)
+    {
+        return this.providedInterfaces.remove(i);
+    }
 }
--- src/fr/univ_nantes/alma/archtool/architectureModel/Interface.java
+++ src/fr/univ_nantes/alma/archtool/architectureModel/Interface.java
@@ -1,7 +1,92 @@
 package fr.univ_nantes.alma.archtool.architectureModel;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import fr.univ_nantes.alma.archtool.sourceModel.Function;
+import fr.univ_nantes.alma.archtool.sourceModel.Type;
+import fr.univ_nantes.alma.archtool.sourceModel.Variable;
+
 
 public class Interface
 {
-    // private Set<CodeEntity> codeEntities;
+    private final Set<Function> functions = new HashSet<Function>();
+
+    private final Set<Variable> variables = new HashSet<Variable>();
+
+    private final Set<Type> types = new HashSet<Type>();
+
+    public Set<Function> getFunctions()
+    {
+        return new HashSet<Function>(this.functions);
+    }
+    
+    public boolean constainsFunction(Function fct)
+    {
+        return this.functions.contains(fct);
+    }
+    
+    public boolean addFunction(final Function fct)
+    {
+        return this.functions.add(fct);
+    }
+
+    public boolean addFunctions(final Set<Function> fcts)
+    {
+        return this.functions.addAll(fcts);
+    }
+    
+    public Set<Variable> getVariables()
+    {
+        return new HashSet<Variable>(this.variables);
+    }
+    
+    public boolean constainsVariable(Function fct)
+    {
+        return this.functions.contains(fct);
+    }
+
+    public boolean addVariable(final Variable var)
+    {
+        return this.variables.add(var);
+    }
+
+    public boolean addVariables(final Set<Variable> vars)
+    {
+        return this.variables.addAll(vars);
+    }
+
+    public Set<Type> getTypes()
+    {
+        return new HashSet<Type>(this.types);
+    }
+
+    public boolean addType(final Type type)
+    {
+        return this.types.add(type);
+    }
+
+    public boolean addTypes(final Set<Type> types)
+    {
+        return this.types.addAll(types);
+    }
+    
+    public boolean equals(Object o)
+    {
+        boolean res = false;
+        
+        if(this.getClass() == o.getClass())
+        {
+            Interface i = (Interface) o;
+            
+            if(this.functions.equals(i.functions)
+                    && this.variables.equals(i.variables)
+                    && this.types.equals(i.types))
+            {
+                res = true;
+            }
+        }
+        
+        return res;
+    }
 }
