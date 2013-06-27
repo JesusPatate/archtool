d7cbd8a93af1680c43b4ed0856cd568b02f97645
Stanislas KOBAK <E071318U@S046V2pc13.ensinfo.sciences.univ-nantes.prive>
2013-06-27 19:07:32 +0200

    grammar works with ANTLR

---
+++ src/fr/univ_nantes/alma/archtool/parsing/TestParser.java
@@ -0,0 +1,19 @@
+package fr.univ_nantes.alma.archtool.parsing;
+
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class TestParser
+{
+    public static void main(String[] args) throws IOException
+    {
+        CharStream s = new ANTLRFileStream("/comptes/E071318U/stage/sou/hr/srclib/hrrjou.c");
+        CLexer lexer = new CLexer(s);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CParser parser = new CParser(tokens);
+        parser.compilationUnit();
+    }
+}
