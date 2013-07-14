d1a631922a1c5d5ebad2f331f74a92fc25da87ee
Jesus Patate <g.a.e.t.a.n@orange.fr>
2013-07-14 18:52:41 +0200

    Code factorisation (Cohesion) + connector cohesion completed

--- src/fr/univ_nantes/alma/archtool/objective/Coupling.java
+++ src/fr/univ_nantes/alma/archtool/objective/Coupling.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 import fr.univ_nantes.alma.archtool.architectureModel.Component;
 import fr.univ_nantes.alma.archtool.architectureModel.Connector;
+import fr.univ_nantes.alma.archtool.architectureModel.Facade;
 import fr.univ_nantes.alma.archtool.architectureModel.Interface;
-import fr.univ_nantes.alma.archtool.sourceModel.Call;
 import fr.univ_nantes.alma.archtool.sourceModel.ComplexType;
 import fr.univ_nantes.alma.archtool.sourceModel.Function;
 import fr.univ_nantes.alma.archtool.sourceModel.GlobalVariable;
@@ -184,6 +184,151 @@ public class Coupling
 
         return result;
     }
+    
+    /**
+     * Évalue le couplage interne d'une facade.
+     * 
+     * @param fcd
+     *            La facade à évaluer
+     * 
+     * @see #coupling(Function, Function)
+     * @see #coupling(Function, Variable)
+     * @see #coupling(Function, ComplexType)
+     * @see #coupling(Variable, ComplexType)
+     */
+    public int facadeCoupling(Facade fcd)
+    {
+        int result = 0;
+
+        Set<Function> functions = fcd.getFunctions();
+        Set<GlobalVariable> variables = fcd.getGlobalVariables();
+        Set<ComplexType> types = fcd.getComplexTypes();
+
+        if (functions.size() > 0)
+        {
+            for (Function fct1 : functions)
+            {
+                for (Function fct2 : functions)
+                {
+                    result += coupling(fct1, fct2);
+                }
+            }
+
+            for (Variable var : variables)
+            {
+                for (Function fct : functions)
+                {
+                    result += coupling(fct, var);
+                }
+            }
+
+            for (ComplexType type : types)
+            {
+                for (Function fct : functions)
+                {
+                    result += coupling(fct, type);
+                }
+            }
+        }
+
+        for (Variable var : variables)
+        {
+            for (ComplexType type : types)
+            {
+                result += coupling(var, type);
+            }
+        }
+
+        return result;
+    }
+    
+    /**
+     * Évalue le couplage entre 2 facades.
+     * 
+     * @param itf
+     *            La facade à évaluer
+     * 
+     * @see #coupling(Function, Function)
+     * @see #coupling(Function, Variable)
+     * @see #coupling(Function, ComplexType)
+     * @see #coupling(Variable, ComplexType)
+     */
+    public int facadesCoupling(Facade fcd1, Facade fcd2)
+    {
+        int result = 0;
+
+        Set<Function> fcd1Fcts = fcd1.getFunctions();
+        Set<GlobalVariable> fcd1Vars = fcd1.getGlobalVariables();
+        Set<ComplexType> fcd1Types = fcd1.getComplexTypes();
+
+        Set<Function> fcd2Fcts = fcd2.getFunctions();
+        Set<GlobalVariable> fcd2Vars = fcd2.getGlobalVariables();
+        Set<ComplexType> fcd2Types = fcd2.getComplexTypes();
+        
+        if (fcd1Fcts.size() > 0)
+        {
+            for (Function fct1 : fcd1Fcts)
+            {
+                for (Function fct2 : fcd2Fcts)
+                {
+                    result += coupling(fct1, fct2);
+                }
+            }
+            
+            for (Function fct1 : fcd1Fcts)
+            {
+                for (GlobalVariable var2 : fcd2Vars)
+                {
+                    result += coupling(fct1, var2);
+                }
+            }
+            
+            for (Function fct1 : fcd1Fcts)
+            {
+                for (ComplexType type2 : fcd2Types)
+                {
+                    result += coupling(fct1, type2);
+                }
+            }
+        }
+        
+        for (GlobalVariable var1 : fcd1Vars)
+        {
+            for (ComplexType type2 : fcd2Types)
+            {
+                result += coupling(var1, type2);
+            }
+        }
+        
+        if (fcd2Fcts.size() > 0)
+        {
+            for (Function fct2 : fcd2Fcts)
+            {
+                for (GlobalVariable var1 : fcd1Vars)
+                {
+                    result += coupling(fct2, var1);
+                }
+            }
+            
+            for (Function fct2 : fcd1Fcts)
+            {
+                for (ComplexType type1 : fcd1Types)
+                {
+                    result += coupling(fct2, type1);
+                }
+            }
+        }
+        
+        for (GlobalVariable var2 : fcd2Vars)
+        {
+            for (ComplexType type1 : fcd1Types)
+            {
+                result += coupling(var2, type1);
+            }
+        }
+        
+        return result;
+    }
 
     /**
      * Mesure le couplage d'une fonction par rapport à une autre.
@@ -204,17 +349,17 @@ public class Coupling
 
         if ((f1 != null) && (f2 != null) && (f1.equals(f2) == false))
         {
-            Set<Call> calls = f1.getTotalCalls();
-            Function fctCalled = null;
-
-            for (Call call1 : calls)
+            Map<Function, Integer> calledF1 = f1.getCoreCalledFunctions();
+            Map<Function, Integer> calledF2 = f2.getCoreCalledFunctions();
+            
+            if(calledF1.containsKey(f2) == true)
             {
-                fctCalled = call1.getFunction();
-
-                if (fctCalled.equals(f2))
-                {
-                    ++result;
-                }
+                result += calledF1.get(f2);
+            }
+            
+            if(calledF2.containsKey(f1) == true)
+            {
+                result += calledF2.get(f1);
             }
         }
 
@@ -244,11 +389,6 @@ public class Coupling
                     .getCoreGlobalVariables();
             
             if (globalVarsFct.containsKey(var))
-            {
-                result = globalVarsFct.get(var);
-            }
-
-            else if (globalVarsFct.containsKey(var))
             {
                 result = globalVarsFct.get(var);
             }
