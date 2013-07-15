596a7720d44ae90cc2aef075ba93f38635f4fd4f
Stan <stan.kobak@gmail.com>
2013-07-15 10:28:20 +0200

    source code builder can take a list of paths

--- src/fr/univ_nantes/alma/archtool/parsing/SourceCodeBuilder.java
+++ src/fr/univ_nantes/alma/archtool/parsing/SourceCodeBuilder.java
@@ -1,6 +1,8 @@
 package fr.univ_nantes.alma.archtool.parsing;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import fr.univ_nantes.alma.archtool.sourceModel.SourceCode;
@@ -10,12 +12,18 @@ public class SourceCodeBuilder
 	private CProcessor cProcessor = new CProcessor();
 	private FileSystemScanner sourcesScanner;
 	private SourceCode sourceCode = new SourceCode();
+	private List<String> paths = null;
 	
 	public SourceCodeBuilder(Set<String> sourceFilePaths,
 			ExtensionFilter sourceFileFilter)
 	{
 	    this.sourcesScanner = 
 	            new FileSystemScanner(sourceFilePaths, sourceFileFilter);
+	}
+	
+	public SourceCodeBuilder(List<String> paths)
+	{
+	    this.paths = paths;
 	}
 	
 	public SourceCodeBuilder(String [] sourceFilePaths,
@@ -27,8 +35,18 @@ public class SourceCodeBuilder
 	
 	public void build()
 	{
-		this.sourcesScanner.scann();	
-		Set<String> sourceFiles = this.sourcesScanner.getPathsFound();
+		Collection<String> sourceFiles = null;
+		
+		if(this.paths != null)
+		{
+		    sourceFiles = this.paths;
+		}
+		else
+		{
+		    this.sourcesScanner.scann();
+		    sourceFiles = this.sourcesScanner.getPathsFound();
+		}
+		
 		Context context = new Context();
 		
 		for(String sourceFile : sourceFiles)
@@ -40,8 +58,10 @@ public class SourceCodeBuilder
 			    context = context.merge(this.cProcessor.getContext());
 			    System.out.println("finishing parse source: " + sourceFile);
 			    this.sourceCode.addFunctions(this.cProcessor.getFunctions());
-			    this.sourceCode.addComplexTypes(this.cProcessor.getComplexTypes());
-			    this.sourceCode.addGlobalVariables(this.cProcessor.getGlobalVariables());
+			    this.sourceCode.addComplexTypes(
+			            this.cProcessor.getComplexTypes());
+			    this.sourceCode.addGlobalVariables(
+			            this.cProcessor.getGlobalVariables());
 			}
 			catch(IOException e)
 			{
